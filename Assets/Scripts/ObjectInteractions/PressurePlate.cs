5f9bd4a11e8f2f7dc7ff4c479ec9d927f1148e28
Sajdens <filipus413@gmail.com>
2025-02-05 07:42:50 +0100

    checkpoint w robieniu leveli
    plus przerwa na zakupy jedzenia i napełnienie baku

--- Assets/Scripts/ObjectInteractions/PressurePlate.cs
+++ Assets/Scripts/ObjectInteractions/PressurePlate.cs
@@ -4,7 +4,7 @@ public class PressurePlate : MonoBehaviour
 {
     public Vector3 _originalPos;
     bool _isPressed = false;
-    private float _maxDownPos = 0.2f;
+    private float _maxDownPos = 0.1f;
 
     [SerializeField] private Transform _target;
     public Vector3 _targetOriginalPos;
@@ -34,20 +34,20 @@ public class PressurePlate : MonoBehaviour
     void Update()
     {
         if(_isPressed){
-            //if(Vector3.Distance(_target.position, _targetOriginalPos) < _targetMaxTransPos){
+            if(Vector3.Distance(_target.position, _targetOriginalPos) < _targetMaxTransPos){
                 _target.Translate(_targetMovePosition.x, _targetMovePosition.y, _targetMovePosition.z);
-            //}
+            }
             if(transform.position.y > _originalPos.y - _maxDownPos){
                 transform.Translate(0.00f, -0.01f, 0.00f);
             }
         }
-        //else if(!_isPressed){
-         //   if(Vector3.Distance(_target.position, _targetOriginalPos) > 0){
-        //        _target.Translate(-_targetMovePosition.x, -_targetMovePosition.y, -_targetMovePosition.z);
-         //   }
-         //   if(transform.position.y < _originalPos.y){
-         //       transform.Translate(0.00f, 0.01f, 0.00f);
-         //   }
-        //}
+        else if(!_isPressed){
+            if(Vector3.Distance(_target.position, _targetOriginalPos) > 0.2){
+                _target.Translate(-_targetMovePosition.x, -_targetMovePosition.y, -_targetMovePosition.z);
+            }
+            if(transform.position.y < _originalPos.y){
+                transform.Translate(0.00f, 0.01f, 0.00f);
+            }
+        }
     }
 }
