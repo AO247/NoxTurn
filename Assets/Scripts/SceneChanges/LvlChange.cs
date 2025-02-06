9ae61d77f5e24a3deaf8c58a1c61a5188206946a
Alamek <162360463+AO247@users.noreply.github.com>
2025-02-06 13:11:48 +0100

    Changing scenes on 'o' and 'p'

--- Assets/Scripts/SceneChanges/LvlChange.cs
+++ Assets/Scripts/SceneChanges/LvlChange.cs
@@ -6,22 +6,31 @@ public class LvlChange : MonoBehaviour
 {
     private bool isPlayerInRange = false; // Czy gracz jest w zasiêgu
     private FADINGCANVAS fadingCanvas;    // Referencja do skryptu FADINGCANVAS
-
+    player player;
     void Start()
     {
         // ZnajdŸ obiekt ze skryptem FADINGCANVAS w scenie
         fadingCanvas = Object.FindFirstObjectByType<FADINGCANVAS>();
+        player = Object.FindFirstObjectByType<player>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         StartCoroutine(ChangeScene());
+        player.canMove = false;
     }
 
 
     private void Update()
     {
-
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            StartCoroutine(ChangeScene());
+        }
+        else if (Input.GetKeyDown(KeyCode.O))
+        {
+            StartCoroutine(ChangeSceneDown());
+        }
     }
 
     private IEnumerator ChangeScene()
@@ -40,6 +49,30 @@ public class LvlChange : MonoBehaviour
             // Za³aduj nastêpn¹ scenê
 
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
+
+        }
+
+    }
+    private IEnumerator ChangeSceneDown()
+    {
+        if (fadingCanvas != null)
+        {
+            fadingCanvas.StartFadeIn(); // Rozpocznij fade-in
+            yield return new WaitForSeconds(2); // Poczekaj na zakoñczenie fade-in
+        }
+        SaveData saveData = new SaveData(); // Utwórz nowy obiekt SaveData
+        if (SceneManager.GetActiveScene().buildIndex - 1 > -1)
+        {
+            saveData.lvlNumber = SceneManager.GetActiveScene().buildIndex - 1;
+            SaveManager.SaveGameState(saveData);
+
+            // Za³aduj nastêpn¹ scenê
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
         }
         else
         {
